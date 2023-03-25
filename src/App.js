138c973642e529c978387e84768e091fdbb29649
Nadya <nadezhdayagubova@gmail.com>
2023-03-25 18:47:29 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import Nav from './components/Nav';
 import SearchBar from './components/SearchBar';
 import Description from './components/Description';
 import Bottom from './components/Bottom';
+import { useState } from 'react';
+
 
 
 function App() {
+const url ="https://api.openweathermap.org/data/2.5/weather?q=Brooklyn&appid=cc0938f5c5704d3c39799aff9a469acc";
+  
+  const [data, setData] = useState({})
+  const [location, setLocation]=useState('')
+
   return (
     <div className="App">
     <Title/>
